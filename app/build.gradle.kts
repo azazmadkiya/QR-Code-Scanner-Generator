c83e1752b8d5302a67ae151c55130e1be7e0d137
azazmadkiya <74822032+azazmadkiya@users.noreply.github.com>
2026-09-19 21:24:25 +0530

    build: update application ID to new namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.qrcodescan.vbfk"
+    applicationId = "com.qrcodescanner.generator.azaz"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
